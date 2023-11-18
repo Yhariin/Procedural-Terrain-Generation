1637c783b787fffb74eb0f883135cc3c251bff16
Yhariin <mr.language77@gmail.com>
2023-11-18 02:13:23 -0500

    Gui Class

---
+++ Gui.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+class Gui
+{
+    public:
+        Gui();
+        ~Gui();
+
+    private:
+
+
+};
